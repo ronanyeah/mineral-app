0792d425d6d83fd7e7edd0a6deccc20a0c64962b
Rónán <ronan_mccabe@protonmail.com>
2024-05-12 15:43:29 -0600

    Worker change

--- src/worker.ts
+++ src/worker.ts
@@ -8,18 +8,17 @@ onmessage = async (event) => {
 };
 
 async function grind(config: MineConfig) {
-  let proof: Uint8Array | null = null;
   let nonce = config.initialNonce || BigInt(0);
-  while (!proof) {
+  while (true) {
     const hash = createHash(config.currentHash, config.signer, nonce);
     if (validateHash(hash, config.difficulty)) {
-      proof = hash;
       const res: MineResult = {
         currentHash: config.currentHash,
         proof: hash,
         nonce,
       };
       postMessage(res);
+      break;
     } else {
       if (nonce % BigInt(1_000_000) == BigInt(0)) {
         postMessage({ checkpoint: nonce, currentHash: config.currentHash });
