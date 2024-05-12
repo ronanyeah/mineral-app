8044c038c8832e4372c23b4a1c9b8f45192fbdf0
Rónán <ronan_mccabe@protonmail.com>
2024-05-12 12:28:37 -0600

    Adjust rpcs

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,11 @@ const WALLET_KEY = "WALLET";
 const RPCS = [
   "https://fullnode.mainnet.sui.io:443",
   "https://mainnet.suiet.app",
-  "https://sui-mainnet-us-2.cosmostation.io",
+  "https://sui-mainnet-us-1.cosmostation.io",
   "https://sui-mainnet-endpoint.blockvision.org",
   "https://sui-mainnet.public.blastapi.io",
   "https://sui-mainnet-rpc.allthatnode.com",
-  "https://sui-mainnet-eu-4.cosmostation.io",
+  "https://sui-mainnet-eu-3.cosmostation.io",
   "https://sui1mainnet-rpc.chainode.tech",
   "https://mainnet.sui.rpcpool.com",
   "https://sui-mainnet-ca-2.cosmostation.io",
